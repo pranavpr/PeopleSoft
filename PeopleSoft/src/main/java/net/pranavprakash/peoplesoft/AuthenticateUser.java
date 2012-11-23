9ad164f637d12331e206b94dd4ac8ce6b8901286
Pranav <pranav.iitkgp@gmail.com>
2012-11-23 13:42:27 +0530

    Completed Authentication Framework

--- PeopleSoft/src/main/java/net/pranavprakash/peoplesoft/AuthenticateUser.java
+++ PeopleSoft/src/main/java/net/pranavprakash/peoplesoft/AuthenticateUser.java
@@ -1,43 +1,81 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.pranavprakash.peoplesoft;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
-/**
- *
- * @author Pranav
- */
 @ManagedBean
 @SessionScoped
-public class AuthenticateUser {
+public class AuthenticateUser
+  implements Serializable
+{
+  String userid;
+  String pwd;
 
-    /**
-     * Creates a new instance of AuthenticateUser
-     */
-    private String userid;
-    private String pwd;
+  public String getUserid()
+  {
+    return this.userid;
+  }
 
-    public String getUserid() {
-        return userid;
-    }
+  public void setUserid(String userid) {
+    this.userid = userid;
+  }
 
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
+  public String getPwd() {
+    return this.pwd;
+  }
 
-    public String getPwd() {
-        return pwd;
-    }
+  public void setPwd(String pwd) {
+    this.pwd = pwd;
+  }
 
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
+  public String doLogin()
+    throws Exception
+  {
+    String result = "pm:loginfailed?transition=flip";
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+    try
+    {
+      HttpPost httpost = new HttpPost("http://www.hrcop.poornata.com:8994/psp/HRCOP/?cmd=login");
+
+      List nvps = new ArrayList();
+      nvps.add(new BasicNameValuePair("userid", this.userid));
+      nvps.add(new BasicNameValuePair("pwd", this.pwd));
+
+      httpost.setEntity(new UrlEncodedFormEntity(nvps, Consts.UTF_8));
+
+      HttpResponse response = httpclient.execute(httpost);
+      HttpEntity entity = response.getEntity();
+      EntityUtils.consume(entity);
+
+      List cookies = httpclient.getCookieStore().getCookies();
+      if ((!cookies.isEmpty()) && (response.getStatusLine().getStatusCode() == 302)) {
+        for (int i = 0; i < cookies.size(); i++) {
+          Cookie pscookie = (Cookie)cookies.get(i);
+          if ((pscookie.getName().equals("PS_TOKEN")) && (pscookie.getDomain().equals("www.hrcop.poornata.com")))
+          {
+            result = "pm:main?transition=flip";
+            break;
+          }
+        }
+      }
+
     }
-    
-    public String doLogin() {
-       return "main";
+    finally
+    {
+      httpclient.getConnectionManager().shutdown();
     }
+    return result;
+  }
 }
